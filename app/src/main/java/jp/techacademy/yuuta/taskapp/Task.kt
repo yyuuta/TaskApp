9da8c86260b46ff9aebab540758f8bf6a65227c4
qbs0005 <yuuta.kawano@qdenbs.com>
2020-07-12 14:23:22 +0900

    Initial commit

---
+++ app/src/main/java/jp/techacademy/yuuta/taskapp/Task.kt
@@ -0,0 +1,16 @@
+package jp.techacademy.yuuta.taskapp
+
+import java.io.Serializable
+import java.util.Date
+import io.realm.RealmObject
+import io.realm.annotations.PrimaryKey
+
+open class Task : RealmObject(), Serializable {
+    var title: String = ""      // タイトル
+    var contents: String = ""   // 内容
+    var date: Date = Date()     // 日時
+
+    // id をプライマリーキーとして設定
+    @PrimaryKey
+    var id: Int = 0
+}
---
+++ app/src/main/java/jp/techacademy/yuuta/taskapp/TaskApp.kt
@@ -0,0 +1,11 @@
+package jp.techacademy.yuuta.taskapp
+
+import android.app.Application
+import io.realm.Realm
+
+class TaskApp: Application() {
+    override fun onCreate() {
+        super.onCreate()
+        Realm.init(this)
+    }
+}
